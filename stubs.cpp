4a5cb41ac5e91c6fbc6fbc9003815914137a7b3f
Eugene V. Lyubimkin <jackyf@iki.fi>
2017-03-26 16:01:17 +0200

    removed obsolete comment

--- stubs.cpp
+++ stubs.cpp
@@ -1,7 +1,6 @@
 #include <stdexcept>
 extern "C" {
 
-//void exception_thrower() __attribute__ ((visibility("hidden")));
 void exception_thrower() __attribute__ ((weak));
 
 void exception_thrower() {
